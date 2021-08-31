4b3e244cbf12986952ac20429c97b9510a9abd4e
Sankarshana V <sankarshana.v@gmail.com>
2021-08-31 12:23:07 +0900

    Adding Assignment 0

---
+++ pa0/src/main/Sorter.java
@@ -0,0 +1,36 @@
+/*
+ * Name: 
+ * Student ID #: 
+ */
+
+/* 
+ * Do NOT import any additional packages/classes.
+ * If you (un)intentionally use some additional packages/classes we did not
+ * provide, you may receive a 0 for the homework.
+ */
+
+public class Sorter implements ISorter {
+	public Sorter() { ; }
+
+	@Override
+	public int[] ascending(int[] a) {
+		/*
+		 * Input:
+		 *  - an integer array A
+		 *
+		 * Output: a sorted array A in *ascending* order.
+		 */
+		return null;
+	}
+	
+	@Override
+	public int[] descending(int[] a) {
+		/*
+		 * Input:
+		 *  - an integer array A
+		 *
+		 * Output: a sorted array A in *descending* order.
+		 */
+		return null;
+	}
+}
