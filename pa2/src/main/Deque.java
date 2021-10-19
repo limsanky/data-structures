21de24976bfe181056f29f84e50cf02f491af843
limsanky <sankarshana.v@gmail.com>
2021-10-19 19:10:00 +0900

    Solved Deque Problem
    Most likely has been solved, but there might be changes to original question file later on.

--- pa2/src/main/Deque.java
+++ pa2/src/main/Deque.java
@@ -15,13 +15,30 @@ public final class Deque<E> implements IDeque<E> {
     * Use some variables for your implementation.
     */
 
+    private int size = 0;
+    private Node<E> head;
+    private Node<E> tail;
+
+    /**
+     * Constructor
+     */
     public Deque() {
         /*
         * Constructor
         * This function is an initializer for this class.
         */
+        head = new Node<>();
+        tail = new Node<>();
+
+        // Connect [head] and [tail] together
+        head.setNext(tail);
+        tail.setPrev(head);
     }
 
+    /**
+     * Adds a new element @item at the front of the Deque
+     * @param item The data of the node added at the beginning of the Deque
+     */
     @Override
     public void insertFirst(E item) {
         /*
@@ -32,8 +49,37 @@ public final class Deque<E> implements IDeque<E> {
         *  Insert the given item before the first item of this deque.
         *  If this deque is empty, use this item as the first item.
         */
+
+        // Create new node to be added at the beginning of the deque,
+        // and set its value to be [item].
+        Node<E> first = new Node<E>();
+        first.setVal(item);
+
+        // Connect [first] to [head] and [tail], if the deque is empty
+        if(isEmpty()){
+            first.setNext(tail);
+            first.setPrev(head);
+
+            tail.setPrev(head);
+            head.setNext(first);
+            return;
+        }
+
+        // Connect [first] to the [head] and previous first element.
+        first.setNext(head.next());
+        first.setPrev(head);
+
+        head.next().setPrev(first);
+        head.setNext(first);
+
+        // Increase size by 1
+        size++;
     }
 
+    /**
+     * Adds a new element @item at the end of the Deque
+     * @param item The data of the node added at the end of the Deque
+     */
     @Override
     public void insertLast(E item) {
         /*
@@ -44,8 +90,34 @@ public final class Deque<E> implements IDeque<E> {
         *  Insert the given item after the last item of this deque.
         *  If this deque is empty, use this item as the last item.
         */
+
+        // Connect [last] to [head] and [tail], if the deque is empty
+        if(isEmpty()) {
+            // It's virtually the same as adding a first node at the beginning, since the list is empty.
+            insertFirst(item);
+            return;
+        }
+
+        // Create new node to be added at the beginning of the deque,
+        // and set its value to be [item].
+        Node<E> last = new Node<E>();
+        last.setVal(item);
+
+        // Connect [last] to the [tail] and previous last element.
+        last.setNext(tail);
+        last.setPrev(tail.prev());
+
+        tail.prev().setNext(last);
+        tail.setPrev(last);
+
+        // Increase size by 1
+        size++;
     }
 
+    /**
+     * Deletes the first element from the Deque
+     * @throws IllegalStateException if the Deque is empty
+     */
     @Override
     public void deleteFirst()
             throws IllegalStateException {
@@ -58,8 +130,21 @@ public final class Deque<E> implements IDeque<E> {
         *  If there is no item, raise an IllegalStateException.
         *  You do not have to specify its message.
         */
+
+        if(isEmpty()) throw new IllegalStateException();
+
+        // Severe connection
+        head.setNext(head.next().next());
+        head.next().setPrev(head);
+
+        // Reduce size by 1
+        size--;
     }
 
+    /**
+     * Deletes the last element from the Deque
+     * @throws IllegalStateException if the Deque is empty
+     */
     @Override
     public void deleteLast()
             throws IllegalStateException {
@@ -72,8 +157,22 @@ public final class Deque<E> implements IDeque<E> {
         *  If there is no item, raise an IllegalStateException.
         *  You do not have to specify its message.
         */
+
+        if(isEmpty()) throw new IllegalStateException();
+
+        // Severe connection
+        tail.prev().prev().setNext(tail);
+        tail.setPrev(tail.prev().prev());
+
+        // Reduce size by 1
+        size--;
     }
 
+    /**
+     * This method returns the data held in the first node
+     * @return Data held in the first node
+     * @throws IllegalStateException if the Deque is empty
+     */
     @Override
     public E first()
             throws IllegalStateException {
@@ -86,9 +185,16 @@ public final class Deque<E> implements IDeque<E> {
         *  If there is no item, raise an IllegalStateException.
         *  You do not have to specify its message.
         */
-        return null;
+        if(isEmpty()) throw new IllegalStateException();
+
+        return (E) head.next().val();
     }
 
+    /**
+     * This method returns the data held in the last node
+     * @return Data held in the last node
+     * @throws IllegalStateException if the Deque is empty
+     */
     @Override
     public E last()
             throws IllegalStateException {
@@ -101,7 +207,9 @@ public final class Deque<E> implements IDeque<E> {
         *  If there is no item, raise an IllegalStateException.
         *  You do not have to specify its message.
         */
-        return null;
+        if(isEmpty()) throw new IllegalStateException();
+
+        return (E) tail.prev().val();
     }
 
     @Override
@@ -112,7 +220,7 @@ public final class Deque<E> implements IDeque<E> {
         * Job:
         *  Return the number of items in the deque.
         */
-        return -1;
+        return size;
     }
 
     @Override
