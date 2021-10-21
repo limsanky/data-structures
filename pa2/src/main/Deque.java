de870f4ab89e905545fad6b6e3cd130aa694318e
limsanky <sankarshana.v@gmail.com>
2021-10-21 16:49:08 +0900

    insertFirst and insertLast Methods have been created

--- pa2/src/main/Deque.java
+++ pa2/src/main/Deque.java
@@ -35,28 +35,85 @@ public final class Deque<E> implements IDeque<E> {
         tail.setPrev(head);
     }
 
+    /**
+     * Adds a new element @item at the front of the Deque
+     * @param item The data of the node added at the beginning of the Deque
+     */
     @Override
     public void insertFirst(E item) {
         /*
-        * Function input:
-        *  item: an item to be inserted.
-        *
-        * Job:
-        *  Insert the given item before the first item of this deque.
-        *  If this deque is empty, use this item as the first item.
-        */
+         * Function input:
+         *  item: an item to be inserted.
+         *
+         * Job:
+         *  Insert the given item before the first item of this deque.
+         *  If this deque is empty, use this item as the first item.
+         */
+
+        // Create new node to be added at the beginning of the deque,
+        // and set its value to be [item].
+        Node<E> first = new Node<E>();
+        first.setVal(item);
+
+        // Connect [first] to [head] and [tail], if the deque is empty
+        if(isEmpty()){
+            first.setNext(tail);
+            first.setPrev(head);
+
+            tail.setPrev(first);
+            head.setNext(first);
+
+            size++;
+            return;
+        }
+
+        // Connect [first] to the [head] and previous first element.
+        first.setNext(head.next());
+        first.setPrev(head);
+
+        head.next().setPrev(first);
+        head.setNext(first);
+
+        // Increase size by 1
+        size++;
     }
 
+    /**
+     * Adds a new element @item at the end of the Deque
+     * @param item The data of the node added at the end of the Deque
+     */
     @Override
     public void insertLast(E item) {
         /*
-        * Function input:
-        *  item: an item to be inserted.
-        *
-        * Job:
-        *  Insert the given item after the last item of this deque.
-        *  If this deque is empty, use this item as the last item.
-        */
+         * Function input:
+         *  item: an item to be inserted.
+         *
+         * Job:
+         *  Insert the given item after the last item of this deque.
+         *  If this deque is empty, use this item as the last item.
+         */
+
+        // Connect [last] to [head] and [tail], if the deque is empty
+        if(isEmpty()) {
+            // It's virtually the same as adding a first node at the beginning, since the list is empty.
+            insertFirst(item);
+            return;
+        }
+
+        // Create new node to be added at the beginning of the deque,
+        // and set its value to be [item].
+        Node<E> last = new Node<E>();
+        last.setVal(item);
+
+        // Connect [last] to the [tail] and previous last element.
+        last.setNext(tail);
+        last.setPrev(tail.prev());
+
+        tail.prev().setNext(last);
+        tail.setPrev(last);
+
+        // Increase size by 1
+        size++;
     }
 
     @Override
