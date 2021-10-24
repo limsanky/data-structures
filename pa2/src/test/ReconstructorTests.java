dcb24c75dca2627b31ab3accdd0a4fa111bf3e18
limsanky <sankarshana.v@gmail.com>
2021-10-24 13:50:26 +0900

    Added more test cases for question 2

--- pa2/src/test/ReconstructorTests.java
+++ pa2/src/test/ReconstructorTests.java
@@ -30,4 +30,58 @@ class ReconstructorTests {
                 assertThat(root.right().parent(), is(root));
             });
     }
+
+    @Test
+    @Score(1)
+    void customTest1()
+    {
+        assertTimeoutPreemptively(
+                Duration.ofSeconds(1), () -> {
+                    IReconstructor tb = new Reconstructor();
+                    assertThrows(
+                            IllegalStateException.class, () -> tb.reconstruct(new Integer[] {1, 2, 4, 3},
+                                    new Integer[] {4,3,1,2}),
+                            "reconstruct()");
+                });
+    }
+
+    @Test
+    @Score(1)
+    void customTest2()
+    {
+        IReconstructor tb = new Reconstructor();
+
+        ITreeNode root = tb.reconstruct(new Integer[] {1},new Integer[] {1});
+        assertThat(root.val(), is(1));
+        assertNull(root.left());
+        assertNull(root.right());
+    }
+
+    @Test
+    @Score(1)
+    void customTest3()
+    {
+        assertTimeoutPreemptively(
+                Duration.ofSeconds(1), () -> {
+                    IReconstructor tb = new Reconstructor();
+                    assertThrows(
+                            IllegalStateException.class, () -> tb.reconstruct(new Integer[] {1, 5},
+                                    new Integer[] {1,2}),
+                            "reconstruct()");
+                });
+    }
+
+    @Test
+    @Score(1)
+    void customTest4()
+    {
+        IReconstructor tb = new Reconstructor();
+
+        ITreeNode root = tb.reconstruct(new Integer[] {9,15,7,20,3},new Integer[] {9,3,15,20,7});
+        assertThat(root.val(), is(3));
+        assertThat(root.left().val(), is(9));
+        assertThat(root.right().val(), is(20));
+        assertThat(root.right().right().val(), is(7));
+        assertThat(root.right().left().val(), is(15));
+    }
 }
