7757f08fcb1afe0eb249bb7c78d03813d3e66fcb
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2021-12-05 01:36:17 +0900

    Created DFS method

--- pa4/src/main/Graph.java
+++ pa4/src/main/Graph.java
@@ -1,7 +1,7 @@
 /*
-* Name:
-* Student ID#:
-*/
+ * Name:
+ * Student ID#:
+ */
 
 import java.io.File;
 import java.io.FileReader;
@@ -9,12 +9,12 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 /*
-* Do NOT use any external packages/classes.
-* If you (un)intentionally use them we did not provide,
-* you will get 0.
-* Also do NOT use auto-import function on IDEs.
-* If the import statements change, you will also get 0.
-*/
+ * Do NOT use any external packages/classes.
+ * If you (un)intentionally use them we did not provide,
+ * you will get 0.
+ * Also do NOT use auto-import function on IDEs.
+ * If the import statements change, you will also get 0.
+ */
 
 public final class Graph implements IGraph {
     private int size;
--- pa4/src/main/SCC.java
+++ pa4/src/main/SCC.java
@@ -13,6 +13,10 @@ import java.util.ArrayList;
 */
 
 public final class SCC implements ISCC {
+    int[][] weightMatrix, transposeMatrix;
+    int numOfVertices;
+    Boolean[] visited;
+
     public SCC() {
         /*
         * Constructor
@@ -27,11 +31,36 @@ public final class SCC implements ISCC {
          *  + G: directed graph
          *  + u: start vertex
          *  + v: end vertex
-         * 
+         *
          * Job:
          *  Return whether there is a path from u to v or not.
          */
         return false;
+    }
+
+    /**
+     * Depth First Search on our graph.
+     * Can be used for transpose matrix as well, but MAKE SURE to have used the [setTranspose()] method first!!
+     * @param source Vertex to visit
+     * @param visited Array of Booleans to see if vertex has been visited
+     * @param forTranspose Boolean to know if DFS should be performed on the transpose matrix or not.
+     */
+    void depthFirstSearch(int source, Boolean[] visited, boolean forTranspose) {
+        visited[source] = true;
+
+        int[] weights;
+
+        if (!forTranspose)
+            weights = weightMatrix[source];
+        else
+            weights = transposeMatrix[source];
+
+        for (int dest = 0; dest < weights.length; dest++) {
+            // Check if weight of edge is not 0, since it means that it's a valid neighbor of [source]
+            if (weights[dest] != 0)
+                if (!visited[dest])
+                    depthFirstSearch(dest, visited, forTranspose);
+        }
     }
 
     @Override
