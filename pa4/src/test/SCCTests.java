79299164df563692d4efccf3da5b56d83bdb72a1
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2021-12-06 20:50:25 +0900

    Added custom test cases and graphs for the assignment!

--- pa4/src/test/SCCTests.java
+++ pa4/src/test/SCCTests.java
@@ -32,6 +32,55 @@ public class SCCTests {
     }
 
     @Test
+    @Score(9)
+    void customTestPath1() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IGraph G = new Graph("./src/test/graph2.txt");
+            ISCC scc = new SCC();
+            assertThat(scc.path(G, 0, 5), is(true));
+            assertThat(scc.path(G, 0, 2), is(true));
+            assertThat(scc.path(G, 0, 3), is(true));
+            assertThat(scc.path(G, 0, 4), is(true));
+            assertThat(scc.path(G, 0, 6), is(true));
+            assertThat(scc.path(G, 0, 7), is(true));
+            assertThat(scc.path(G, 1, 6), is(true));
+            assertThat(scc.path(G, 2, 5), is(true));
+            assertThat(scc.path(G, 6, 9), is(true));
+        });
+    }
+
+    @Test
+    @Score(5)
+    void customTestPath2() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IGraph G = new Graph("./src/test/graph5.txt");
+            ISCC scc = new SCC();
+            assertThat(scc.path(G, 0, 1), is(true));
+            assertThat(scc.path(G, 3, 0), is(true));
+            assertThat(scc.path(G, 1, 0), is(true));
+            assertThat(scc.path(G, 4, 3), is(true));
+        });
+    }
+
+    @Test
+    @Score(8)
+    void customTestPath3() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IGraph G = new Graph("./src/test/graph4.txt");
+            ISCC scc = new SCC();
+            assertThat(scc.path(G, 3, 0), is(false));
+            assertThat(scc.path(G, 3, 1), is(false));
+            assertThat(scc.path(G, 4, 1), is(false));
+            assertThat(scc.path(G, 2, 1), is(false));
+            assertThat(scc.path(G, 0, 4), is(false));
+            assertThat(scc.path(G, 0, 2), is(false));
+
+            assertThat(scc.path(G, 1, 0), is(true));
+            assertThat(scc.path(G, 3, 4), is(true));
+        });
+    }
+
+    @Test
     @Score(1)
     void testconnectivity() {
         assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
@@ -42,6 +91,20 @@ public class SCCTests {
             assertThat(scc.connectivity(G2), is(10));
             assertThat(scc.connectivity(G3), is(1));
             assertThat(scc.connectivity(G4), is(2));
+        });
+    }
+
+    @Test
+    @Score(2)
+    void customTestConnectivity1() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1), () -> {
+            IGraph G1 = new Graph("./src/test/graph1.txt");
+            IGraph G5 = new Graph("./src/test/graph5.txt");
+
+            ISCC scc = new SCC();
+
+            assertThat(scc.connectivity(G1), is(1));
+            assertThat(scc.connectivity(G5), is(1));
         });
     }
 }
