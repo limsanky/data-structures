d8dfc82dd9cf53af89cd8a85b6c99f1519a0c390
Sankarshana V <61644684+limsanky@users.noreply.github.com>
2021-12-08 19:11:20 +0900

    Change the add operator to addNumbers() method instead
    Since Integer.MAX_VALUE can be one of the inputs, we should rather use the custom addNumbers() function.

--- pa4/src/main/MST.java
+++ pa4/src/main/MST.java
@@ -163,7 +163,8 @@ public final class MST implements IMST {
         // Create the undirected graph edges from the input graph.
         for (int s = 0; s < numOfVertices; s++) {
             for (int i = s + 1; i < numOfVertices; i++) {
-                int temp = matrix[s][i] + matrix[i][s];
+                int temp = addNumbers(matrix[s][i], matrix[i][s]);
+//                int temp = matrix[s][i] + matrix[i][s];
 
                 undirectedEdges[s][i] = temp;
                 undirectedEdges[i][s] = temp;
